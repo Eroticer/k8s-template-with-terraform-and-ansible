375b46f40dbfb9da87c32090152c8f361d81812f
alexboytsov53 <alexboytsov53@gmail.com>
2026-08-16 19:03:09 +0300

    Fix main.tf lang

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,44 +1,45 @@
-data "beget_image" "ubuntu" {
-	name = "Ubuntu 22.04 LTS"
+data "beget_software" "ubuntu" {
+	slug = "ubuntu-24-04"
 }
 
-resource "beget_ssh_key" "k8s_key" {
+resource "beget_ssh_key" "k8s" {
   name = "k8s-ssh-key"
   public_key = file("~/.ssh/id_ed25519.pub")
 }
 
 resource "beget_compute_instance" "master" {
 	count = var.master_count
+
 	name = "k8s-master-${count.index}"
 	description = "Master node for cubernetes cluster"
 	hostname = "k8s-master-${count.index}"
 	region = "ru1"
-	image_id = data.beget_image.ubuntu.id
-	ssh_key_ids = [beget_ssh_key.k8s_key_id]
 
 	configuration = {
 		cpu = 2
-		ram_mb = 4 * 1024
-		disk_mb = 50 * 1024
+		ram_mb = 4096
+		disk_mb = 51200
 		cpu_class = "normal_cpu"
 	}
 
-	labels = {
-		role = "master"
-		cluster = "k8s"
-		env = "production"
-		managed_by = "terraform"
+	image = {
+		software = {
+			id = data.beget_software.ubuntu.id
+		}
+	}
+
+	access = {
+		ssh_keys = [beget_ssh_key.k8s.id]
 	}
 }
 
 resource "beget_compute_instance" "worker" {
 	count = var.worker_count
+
 	name = "k8s-worker-${count.index}"
 	description = "Worker node for Kubernetes cluster"
 	hostname = "k8s-worker-${count.index}"
 	region = "ru1"
-	image_id = data.beget_image.ubuntu.id
-	ssh_keys = [beget_ssh_key.k8s_key_id]
 
 	configuration = {
 		cpu = 2
@@ -47,10 +48,13 @@ resource "beget_compute_instance" "worker" {
 		cpu_class = "normal_cpu"
 	}
 
-	labels = {
-		role = "worker"
-		cluser = "k8s"
-		env = "production"
-		manage_by = "terraform"
+	image = {
+		software = {
+			id = data.beget_software.ubuntu.id
+		}
+	}
+
+	access = {
+		ssh_keys = [beget_ssh_key.k8s.id]
 	}
 }
