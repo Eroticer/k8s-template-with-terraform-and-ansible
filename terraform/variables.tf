fd8b1f04dbe504e4ea5eb155e592191c763717f6
alexboytsov53 <alexboytsov53@gmail.com>
2026-08-16 09:45:14 +0300

    fix word

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,7 +17,7 @@ variable "worker_count" {
 }
 
 variable "ssh_public_key_path" {
-  description = "Path to SSH pablic key"
+  description = "Path to SSH public key"
   type = string
   default = "~/.ssh/id_ed25519.pub"
 }
